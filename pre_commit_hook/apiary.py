ca72e5786fb51d426bef11613e11a0f318240011
benjamin99 <cknomber1@gmail.com>
2015-07-13 21:10:42 +0800

    [update] complete the implementation of the validator and added the decorder object for the validation process

--- pre_commit_hook/apiary.py
+++ pre_commit_hook/apiary.py
@@ -1,10 +1,12 @@
 __author__ = 'Arsenal_49'
 
 import re
+from pre_commit_hook.decoder import ApiDecoder as ContentDecoder
+from pre_commit_hook.error import ApiarySyntaxError
 
 # Define the pattern match/search
 _group_title    = re.compile(r'^#(\s)+.*').match
-_api_title      = re.compile(r'^##.*\[\/.+\]$').match
+_api_title      = re.compile(r'^##(\s)+.*\[\/.+\]$').match
 _api_url        = re.compile(r'\[\/[a-zA-Z0-9\ \_\-\/\{/}]*[\{\]]').search
 _api_method     = re.compile(r'\[(GET|POST|PUT|DELETE|PATCH)\]').search
 _param_title    = re.compile(r'^\+ Parameters').match
@@ -20,12 +22,15 @@ _state_read_api_method      = 3
 _state_read_param_tag       = 4
 _state_read_request_tag     = 5
 _state_read_response_tag    = 6
+_state_error                = -1
 
 # ----------------------------------------------------------------------------------------------------------------------
 # define the Validator class
 class ApiaryValidator:
     def __init__(self):
         self.state = _state_init
+        self.decoder = ContentDecoder()
+        self._read_parameter_string = False
 
     def validate_file(self, file):
         validation_result = True
@@ -47,30 +52,116 @@ class ApiaryValidator:
                 print('ValError: %s (@ %d)' % (error.message, line_count))
                 validation_result = False
                 break
+
         return validation_result
 
     def _read_line(self, line):
         error = None
+        assert self.state > 0, 'StateError: the validator is in the error state'
 
         if self.state == _state_read_group_title:
-            pass
+            if _api_title(line):
+                if not _api_url(line):
+                    error = ApiarySyntaxError(message='Cannot find the api url in line: %s' % line)
+                    self.state = _state_error
+                else:
+                    self.state = _state_read_api_title
+
+            elif _api_method(line) or _param_title(line) or _param_string(line) or _request_title(line) \
+                    or _response_title(line):
+                error = ApiarySyntaxError(message='Missing api title string')
+                self.state = _state_error
 
         elif self.state == _state_read_api_title:
-            pass
+            if _api_method(line):
+                self.state = _state_read_api_method
+
+            elif _param_title(line) or _param_string(line) or _request_title(line) or _response_title(line):
+                error = ApiarySyntaxError(message='Missing api method string')
+                self.state = _state_error
 
         elif self.state == _state_read_api_method:
-            pass
+            if _param_title(line):
+                self._read_parameter_string = False
+                self.state = _state_read_param_tag
+
+            elif _request_title(line):
+                self.decoder.clear()
+                self.state = _state_read_request_tag
+
+            elif _response_title(line):
+                self.decoder.clear()
+                self.state = _state_read_response_tag
+
+            elif _param_string(line):
+                error = ApiarySyntaxError(message='Missing parameter title')
+                self.state = _state_error
 
         elif self.state == _state_read_param_tag:
-            pass
+            if _param_string(line):
+                self._read_parameter_string = True
+
+            elif _request_title(line):
+                if not self._read_parameter_string:
+                    error = ApiarySyntaxError(message='Missing parameter info')
+                    self.state = _state_error
+                else:
+                    self.decoder.clear()
+                    self.state = _state_read_request_tag
+
+            elif _response_title(line):
+                if not self._read_parameter_string:
+                    error = ApiarySyntaxError(message='Missing parameter info')
+                    self.state = _state_error
+                else:
+                    self.decoder.clear()
+                    self.state = _state_read_response_tag
+
+            elif not re.match(r'\s+', line):
+                error = ApiarySyntaxError(message='The lines should contain the parameter info')
+                self.state = _state_error
 
         elif self.state == _state_read_request_tag:
-            pass
+            if _response_title(line):
+                if not self.decoder.get_parsed_objects():
+                    error = ApiarySyntaxError(message='Missing request content')
+                    self.state = _state_error
+                else:
+                    self.decoder.clear()
+                    self.state = _state_read_response_tag
+            else:
+                try:
+                    self.decoder.scan_line(line)
+                except Exception as e:
+                    error = ApiarySyntaxError(message='DecodeException: %s' % e.message)
+                    self.state = _state_error
 
         elif self.state == _state_read_response_tag:
-            pass
+            if _group_title(line):
+                if not self.decoder.get_parsed_objects():
+                    error = ApiarySyntaxError(message='Missing response content')
+                    self.state = _state_error
+                else:
+                    self.decoder.clear()
+                    self.state = _state_read_group_title
+
+            elif _api_title(line):
+                if not self.decoder.get_parsed_objects():
+                    error = ApiarySyntaxError(message='Missing response content')
+                    self.state = _state_error
+                else:
+                    self.decoder.clear()
+                    self.state = _state_read_api_title
+
+            else:
+                try:
+                    self.decoder.scan_line(line)
+                except Exception as e:
+                    error = ApiarySyntaxError(message='DecodeException: %s' % e.message)
+                    self.state = _state_error
 
         else:   # _state_init
-            pass
+            if _group_title(line):
+                self._state = _state_read_group_title
 
         return (error is None), error
